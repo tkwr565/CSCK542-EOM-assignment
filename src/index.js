c4bcd577f94bf54d724956018934061ec7089cb6
shingchan95 <chanhei888@gmail.com>
2023-12-06 22:40:31 +0000

    update on express

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const routes = require('./routes/index');
+
+
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.status(200);
     res.send('DB access obtained')
 });
 
+
 app.listen(port, (error) => {
-    if(!error)
-        console.log("Server is successfully running, app is listening on port ${port}")
+    if (!error)
+        console.log(`Server is successfully running, app is listening on port ${port}`)
     else
         console.log("Error occurred, server cannot start", error);
 });

030ae61b4318b31c120a49a606e6e108ea99c290
husky960 <youtmu@gmail.com>
2023-12-06 14:27:05 +0100

    implemented database.js and testdb.js. database.js handles the connection to the database using node.js. testdb.js tests database.js. Additionally .env variable was introduced to handle the mysql information.

---
+++ src/models/testdb.js
@@ -0,0 +1,16 @@
+//require('dotenv').config({path: '../../.env'});
+require('dotenv').config({path: '../.env'});
+const connection = require('../config/database');
+
+// Sample query
+connection.query('SELECT * FROM courses', (err, results, fields) => {
+    if (err) {
+        console.error('An error occurred while executing the query: ' + err.stack);
+        return;
+    }
+
+    console.log('Results: ', results);
+});
+
+// Close the connection
+connection.end();
